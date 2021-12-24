8b7b5fcba1003398bff76e8c3550b5c9501b3f9f
StoilTomanov <stoiltomanov@gmail.com>
2021-12-24 23:35:10 +0200

    edited home view

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import page from './lib/page.mjs';
 import { showHome } from './views/homeView.js';
+import { showSignIn } from './views/signInView.js';
 
 export const main = document.getElementById('main');
+export const body = document.querySelector('html');
 
-page.redirect('/')
+page.redirect('/');
 page('/', showHome);
+page('/signin', showSignIn);
 
 page.start();
---
+++ src/views/signInView.js
@@ -0,0 +1,17 @@
+import { body, main } from '../app.js';
+import { html, render } from '../lib/lit-html.js';
+
+export function showSignIn() {
+    body.style.backgroundColor = 'white';
+    const template = html `
+    <section id="loginView">
+        <h3>Sign In</h3>
+        <button class="type">Organization</button>
+        <button class="type">Individual</button>
+        <div class="container signin">
+            <p>Dont have an account?<a href="#">Sign up</a>.</p>
+        </div>
+    </section>
+    `
+    render(template, main);
+}
