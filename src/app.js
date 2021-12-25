badddbb42dc909ec7653dcc7db27b03fec3dd59e
StoilTomanov <stoiltomanov@gmail.com>
2021-12-25 23:38:12 +0200

    start dashboard view

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-import page from './lib/page.mjs';
-import { showHome } from './views/homeView.js';
-import { showSignIn } from './views/signInView.js';
-import { showSignUp } from './views/signupView.js';
+// import page from './lib/page.mjs';
+// import { showHome } from './views/homeView.js';
+// import { showSignIn } from './views/signInView.js';
+// import { showSignUp } from './views/signupView.js';
 
-export const main = document.getElementById('main');
-export const body = document.querySelector('html');
+// export const main = document.getElementById('main');
+// export const body = document.querySelector('html');
 
-page('/', showHome);
-page('/signin', showSignIn);
-page('/signup', showSignUp);
+// page('/', showHome);
+// page('/signin', showSignIn);
+// page('/signup', showSignUp);
 
-page.start();
+// page.start();
---
+++ src/views/dashboardView.js
@@ -0,0 +1,14 @@
+import { body, main } from '../app.js';
+import { onBack } from '../controllers/backBtn.js';
+import { onTypeChoice } from '../controllers/typeBtn.js';
+import { html, render } from '../lib/lit-html.js';
+
+export function showSignIn() {
+    body.style.background = 'rgb(255, 255, 255)';
+    const template = html `
+    <section id="dashboardView">
+
+    </section>
+    `
+    render(template, main);
+}
