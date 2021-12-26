584e3c6f6153ae493b81ae4e9824a5b2bc5e8edc
StoilTomanov <stoiltomanov@gmail.com>
2021-12-26 13:48:18 +0200

    navbar

---
+++ src/controllers/navbar.js
@@ -0,0 +1,8 @@
+export function toggleNav() {
+    const li = document.querySelectorAll('[class="navLi"]');
+    const toggleOpen = document.getElementById('toggleOpen');
+    const toggleClose = document.getElementById('toggleClose');
+
+    //TODO: show / hide navigation
+
+}
--- src/views/navigationView.js
+++ src/views/navigationView.js
@@ -5,9 +5,9 @@ export function navigationBar() {
     <nav id="navbar">
         <button id="toggleOpen">&#9776;</button>
         <button id="toggleClose">&#10006;</button>
-        <li><img src="./img/dashboard.png" alt="dashboard" class="img" id="dashboard"><a href="/dashboard">Dashboard</a></li>
-        <li><img src="./img/team.png" alt="team" class="img" id="team"><a href="/myteam">My Team</a></li>
-        <li><img src="./img/settings.png" alt="settings" class="img" id="settings"><a href="/settings">Settings</a></li>
+        <li class="navLi"><img src="./img/dashboard.png" alt="dashboard" class="img" id="dashboard"><a href="/dashboard">Dashboard</a></li>
+        <li class="navLi"><img src="./img/team.png" alt="team" class="img" id="team"><a href="/myteam">My Team</a></li>
+        <li class="navLi"><img src="./img/settings.png" alt="settings" class="img" id="settings"><a href="/settings">Settings</a></li>
     </nav>
     `
 }
