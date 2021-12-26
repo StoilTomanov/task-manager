0d126339cdd698fe285b1db0e9c33bc6088d473d
StoilTomanov <stoiltomanov@gmail.com>
2021-12-26 18:16:14 +0200

    added team view

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-import page from './lib/page.mjs';
-import { showDashboard } from './views/dashboardView.js';
-import { showHome } from './views/homeView.js';
-import { showSignIn } from './views/signInView.js';
-import { showSignUp } from './views/signupView.js';
+// import page from './lib/page.mjs';
+// import { showDashboard } from './views/dashboardView.js';
+// import { showHome } from './views/homeView.js';
+// import { showSignIn } from './views/signInView.js';
+// import { showSignUp } from './views/signupView.js';
 
-export const main = document.getElementById('main');
-export const body = document.querySelector('html');
+// export const main = document.getElementById('main');
+// export const body = document.querySelector('html');
 
-page('/', showHome);
-page('/signin', showSignIn);
-page('/signup', showSignUp);
-page('/dashboard', showDashboard);
-page('/myteam', showDashboard);
-page('/settings', showDashboard);
+// page('/', showHome);
+// page('/signin', showSignIn);
+// page('/signup', showSignUp);
+// page('/dashboard', showDashboard);
+// page('/myteam', showDashboard);
+// page('/settings', showDashboard);
 
-page.start();
+// page.start();
