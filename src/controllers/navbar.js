eb3e8bbfc7cf57f2859de20f077c7ea2c0c323fe
StoilTomanov <stoiltomanov@gmail.com>
2021-12-26 14:33:46 +0200

    toggle bar created

--- src/controllers/navbar.js
+++ src/controllers/navbar.js
@@ -1,8 +1,28 @@
-export function toggleNav() {
+import { main } from "../app.js";
+
+export function toggleNav(ev) {
     const li = document.querySelectorAll('[class="navLi"]');
     const toggleOpen = document.getElementById('toggleOpen');
     const toggleClose = document.getElementById('toggleClose');
+    const navigation = document.querySelector('[id="navbar"]');
 
-    //TODO: show / hide navigation
+    if (ev.target.id == 'toggleOpen') {
+        toggleOpen.style.display = 'none';
+        toggleClose.style.display = 'inline-block';
+
+        li.forEach(item => item.style.display = 'inline-block');
+        navigation.style.width = '16%';
+        main.style.marginLeft = "18%";
+
+    } else if (ev.target.id == 'toggleClose') {
+        toggleOpen.style.display = 'inline-block';
+        toggleClose.style.display = 'none';
+
+        li.forEach(item => item.style.display = 'none');
+        navigation.style.width = '5%';
+        main.style.marginLeft = "7%";
+
+    }
+    //TODO: fix the show / hide nav for phone resolution
 
 }
--- src/views/navigationView.js
+++ src/views/navigationView.js
@@ -1,10 +1,11 @@
+import { toggleNav } from '../controllers/navbar.js';
 import { html } from '../lib/lit-html.js';
 
 export function navigationBar() {
     return html `
     <nav id="navbar">
-        <button id="toggleOpen">&#9776;</button>
-        <button id="toggleClose">&#10006;</button>
+        <button id="toggleOpen" @click=${toggleNav}>&#9776;</button>
+        <button id="toggleClose" @click=${toggleNav}>&#10005;</button>
         <li class="navLi"><img src="./img/dashboard.png" alt="dashboard" class="img" id="dashboard"><a href="/dashboard">Dashboard</a></li>
         <li class="navLi"><img src="./img/team.png" alt="team" class="img" id="team"><a href="/myteam">My Team</a></li>
         <li class="navLi"><img src="./img/settings.png" alt="settings" class="img" id="settings"><a href="/settings">Settings</a></li>
