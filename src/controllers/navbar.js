07595d1bf04d4db7c2446db84d1c61c1d3ca3553
StoilTomanov <stoiltomanov@gmail.com>
2021-12-26 15:29:14 +0200

    created dashboard view

--- src/controllers/navbar.js
+++ src/controllers/navbar.js
@@ -11,16 +11,28 @@ export function toggleNav(ev) {
         toggleClose.style.display = 'inline-block';
 
         li.forEach(item => item.style.display = 'inline-block');
-        navigation.style.width = '16%';
-        main.style.marginLeft = "18%";
+        if (window.screen.width < 600) {
+            navigation.style.width = '35%';
+            main.style.marginLeft = "0%";
+        } else if (window.screen.width > 600) {
+            navigation.style.width = '16%';
+            main.style.marginLeft = "0%";
+        }
+
 
     } else if (ev.target.id == 'toggleClose') {
         toggleOpen.style.display = 'inline-block';
         toggleClose.style.display = 'none';
 
         li.forEach(item => item.style.display = 'none');
-        navigation.style.width = '5%';
-        main.style.marginLeft = "7%";
+        if (window.screen.width < 600) {
+            navigation.style.width = '8%';
+            main.style.marginLeft = "-42%";
+        } else if (window.screen.width > 600) {
+            navigation.style.width = '5%';
+            main.style.marginLeft = "-14%";
+        }
+
 
     }
     //TODO: fix the show / hide nav for phone resolution
