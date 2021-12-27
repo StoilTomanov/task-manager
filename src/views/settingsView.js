073ee63acfe32852f7637e15bd00dad4be26108b
StoilTomanov <stoiltomanov@gmail.com>
2021-12-28 00:14:07 +0200

    settings

--- src/views/settingsView.js
+++ src/views/settingsView.js
@@ -13,24 +13,24 @@ export function showSettings() {
         </div>
         <div>
             <form id="settings">
-                <div>
-                    <label for="toggle-dark">Light</label>
+                <div id="toggle">
+                <label for="toggle-dark">Light</label>
                     <input type="checkbox" id="toggle-btn">
                     <label for="toggle-light">Dark</label>
                 </div>
-                <div>
-                    <label for="username">Change username</label>
-                    <input type="text" name="username" id="username" class="change-value">
+                <div class="box">
+                    <label for="username" class="change-value">Change username</label>
+                    <input type="text" name="username" id="username">
                 </div>
-                <div>
-                    <label for="teamName">Change Team name</label>
-                    <input type="text" name="teamName" id="teamName" class="change-value">
+                <div class="box">
+                    <label for="teamName" class="change-value">Change Team name</label>
+                    <input type="text" name="teamName" id="teamName">
                 </div>
-                <div>
-                    <label for="teamName">Change password</label>
-                    <input type="password" name="old-pass" id="old-pass" class="change-value" placeholder="Old password">
-                    <input type="password" name="new-pass" id="old-pass" class="change-value" placeholder="New password">
-                    <input type="password" name="re-pass" id="old-pass" class="change-value" placeholder="Repeat password">
+                <div class="box">
+                    <label for="teamName" class="change-value">Change password</label>
+                    <input type="password" name="old-pass" id="old-pass" placeholder="Old password">
+                    <input type="password" name="new-pass" id="old-pass" placeholder="New password">
+                    <input type="password" name="re-pass" id="old-pass" placeholder="Repeat password">
                 </div>
                 <div>
                     <input type="submit" value="Apply" class="applyBtn">
