abbdfaa946c58c0daa0e70c10598f5241fc172e3
StoilTomanov <stoiltomanov@gmail.com>
2021-12-27 12:18:06 +0200

    views

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import page from './lib/page.mjs';
 import { showDashboard } from './views/dashboardView.js';
 import { showHome } from './views/homeView.js';
-import { showSignIn } from './views/signInView.js';
-import { showSignUp } from './views/signupView.js';
+import { showSignIn } from './views/loginView.js';
+import { showSignUp } from './views/registerView.js';
 import { showTeam } from './views/teamView.js';
 
 export const main = document.getElementById('main');
