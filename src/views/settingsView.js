0d5526342614b01d5534d93ff6d5a51556a8bb33
StoilTomanov <stoiltomanov@gmail.com>
2021-12-28 11:44:21 +0200

    style

--- src/views/settingsView.js
+++ src/views/settingsView.js
@@ -14,20 +14,21 @@ export function showSettings() {
         <div>
             <form id="settings">
                 <div id="toggle">
-                <label for="toggle-dark">Light</label>
+                <label class="change-value">Change theme</label>
+                <label for="toggle-dark" class="toggle-txt">Light</label>
                     <input type="checkbox" id="toggle-btn">
-                    <label for="toggle-light">Dark</label>
+                <label for="toggle-light" class="toggle-txt">Dark</label>
                 </div>
-                <div class="box">
+                <div class="box" id="ch-username">
                     <label for="username" class="change-value">Change username</label>
                     <input type="text" name="username" id="username">
                 </div>
-                <div class="box">
+                <div class="box" id="ch-teamName">
                     <label for="teamName" class="change-value">Change Team name</label>
                     <input type="text" name="teamName" id="teamName">
                 </div>
-                <div class="box">
-                    <label for="teamName" class="change-value">Change password</label>
+                <div class="box" id="ch-password">
+                    <label for="password" class="change-value">Change password</label>
                     <input type="password" name="old-pass" id="old-pass" placeholder="Old password">
                     <input type="password" name="new-pass" id="old-pass" placeholder="New password">
                     <input type="password" name="re-pass" id="old-pass" placeholder="Repeat password">
