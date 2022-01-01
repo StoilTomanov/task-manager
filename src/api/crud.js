590c96f95f0824cb039f24b8df305266956b80f0
StoilTomanov <stoiltomanov@gmail.com>
2022-01-01 23:52:54 +0200

    added endpoints

--- src/api/crud.js
+++ src/api/crud.js
@@ -1,3 +1,17 @@
+import { endpoint } from "./endpoints.js";
+
 const appID = 'ojCxQpaTc8ay4TdoBxRNrrdkBpAANgEGFjJjKVia';
 const apiKey = '71aS9FE1hAsE9bN526INsJhpRbwKjBxxI5fX7p68';
 const contentType = 'application/json';
+
+export function login(userData) {
+
+}
+
+// export function register(userData) {
+
+// }
+
+// export function logout(userData) {
+
+// }
---
+++ src/api/endpoints.js
@@ -0,0 +1,7 @@
+const host = 'https://parseapi.back4app.com'
+
+export const endpoint = {
+    register: host + '/users',
+    login: host + '/login',
+    logout: host + '/logout',
+}
