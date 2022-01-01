28ec615d4031af3a1e414549935eaad796ef1cee
StoilTomanov <stoiltomanov@gmail.com>
2022-01-02 00:05:29 +0200

    added crud operations

--- src/api/crud.js
+++ src/api/crud.js
@@ -6,20 +6,37 @@ const contentType = 'application/json';
 
 export async function login(userData) {
     const repsonse = await fetch(endpoint.login, {
+        method: 'get',
+        headers: {
+            'X-Parse-Application-Id': appID,
+            'X-Parse-REST-API-Key': apiKey,
+            'X-Parse-Revocable-Session': 1,
+        },
+        body: JSON.stringify(userData),
+    });
+}
+
+export function register(userData) {
+    const repsonse = await fetch(endpoint.register, {
         method: 'post',
         headers: {
             'X-Parse-Application-Id': appID,
             'X-Parse-REST-API-Key': apiKey,
+            'X-Parse-Revocable-Session': 1,
             'Content-Type': contentType,
-        }
+        },
+        body: JSON.stringify(userData),
     });
-
 }
 
-// export function register(userData) {
-
-// }
-
-// export function logout(userData) {
-
-// }
+export function logout(userData) {
+    const repsonse = await fetch(endpoint.logout, {
+        method: 'post',
+        headers: {
+            'X-Parse-Application-Id': appID,
+            'X-Parse-REST-API-Key': apiKey,
+            'X-Parse-Session-Token': sessionStorage, // session token here
+        },
+        body: JSON.stringify(userData),
+    });
+}
