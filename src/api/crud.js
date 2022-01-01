9f7cba93515b7a5a1301d1d306f0e9ccdd93dd2f
StoilTomanov <stoiltomanov@gmail.com>
2022-01-01 23:57:10 +0200

    added crud

--- src/api/crud.js
+++ src/api/crud.js
@@ -4,7 +4,15 @@ const appID = 'ojCxQpaTc8ay4TdoBxRNrrdkBpAANgEGFjJjKVia';
 const apiKey = '71aS9FE1hAsE9bN526INsJhpRbwKjBxxI5fX7p68';
 const contentType = 'application/json';
 
-export function login(userData) {
+export async function login(userData) {
+    const repsonse = await fetch(endpoint.login, {
+        method: 'post',
+        headers: {
+            'X-Parse-Application-Id': appID,
+            'X-Parse-REST-API-Key': apiKey,
+            'Content-Type': contentType,
+        }
+    });
 
 }
 
