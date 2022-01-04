4095391207015e4e2c6e17ee9e6ab8059dc53180
StoilTomanov <stoiltomanov@gmail.com>
2022-01-04 19:24:55 +0200

    app

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-// import page from './lib/page.mjs';
-// import { showDashboard } from './views/dashboardView.js';
-// import { showHome } from './views/homeView.js';
-// import { showSignIn } from './views/loginView.js';
-// import { showSignUp } from './views/registerView.js';
-// import { showSettings } from './views/settingsView.js';
-// import { showTeam } from './views/teamView.js';
-// import uuid from './lib/v4.js'
+import page from './lib/page.mjs';
+import { showDashboard } from './views/dashboardView.js';
+import { showHome } from './views/homeView.js';
+import { showSignIn } from './views/loginView.js';
+import { showSignUp } from './views/registerView.js';
+import { showSettings } from './views/settingsView.js';
+import { showTeam } from './views/teamView.js';
+import uuid from './lib/v4.js'
 
-// console.log(uuid());
+console.log(uuid());
 
-// //TODO: create a view after login/registration to inform the unique team identifier and ask about the organization name
+//TODO: create a view after login/registration to inform the unique team identifier and ask about the organization name
 
-// export const main = document.getElementById('main');
-// export const body = document.querySelector('html');
+export const main = document.getElementById('main');
+export const body = document.querySelector('html');
 
-// page('/home', '/');
-// page('/', showHome);
-// page('/signin', showSignIn);
-// page('/signup', showSignUp);
-// page('/dashboard', showDashboard);
-// page('/myteam', showTeam);
-// page('/settings', showSettings);
+page('/home', '/');
+page('/', showHome);
+page('/signin', showSignIn);
+page('/signup', showSignUp);
+page('/dashboard', showDashboard);
+page('/myteam', showTeam);
+page('/settings', showSettings);
 
-// page.start();
+page.start();
